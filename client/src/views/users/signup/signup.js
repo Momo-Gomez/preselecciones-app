b1015cbb3f85f99d4f414cc234a248f8e1324f87
Conejo <vf.araya.a@gmail.com>
2022-05-25 13:02:40 -0400

    Agregando labels y borrando error de guardado

---
+++ client/src/views/users/signup/signup.js
@@ -0,0 +1,15 @@
+import "./signup.css";
+
+export default function signup(){
+    return (
+        <div className="signup">
+            <span className="signup-title">Registro</span>
+            <form className="signup-form">
+                
+                <label>Nombre</label>
+
+            </form>
+
+        </div>
+    )
+}
