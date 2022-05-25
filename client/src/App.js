97faf290e6c8adc59d6ecf1ea1b83340721aa1f3
Ragnalex <66427660+Ragnalex@users.noreply.github.com>
2022-05-25 11:44:25 -0400

    Inicializacion client y serv

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Fragment} from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <div>
+        <div className='container '>
+          <div>
+            <div className='home-background border border-dark border-opacity-25'>
+              <div className='btn-group d-flex flex-row-reverse'>
+                  <div className='mr-3'>
+                    asfasfgasgfas
+                  </div>
+                  <div className='ml-3'>
+                    asdafasda
+                  </div>
+                
+                
+              </div>
+                
+            </div>
+            
+            <div className="background-search mb-3 border border-dark border-opacity-25">
+                    
+                    <h1 className="text-center mb-3"> Busca aqui tu beneficio </h1>
+                    <div className='mb-3'>
+                      probando
+                    </div>
+                
+                  
+            </div>
+          </div>
+        </div>
+      </div>
+      
+        
+      
+    </Fragment>
   );
 }
 
