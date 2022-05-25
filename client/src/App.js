bfdc2a79598900bf249a35fb60a2ebfa2bcb5503
Ragnalex <66427660+Ragnalex@users.noreply.github.com>
2022-05-25 12:22:02 -0400

    todo creado para el front (En views van las Pags)

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,20 @@ import './App.css';
 import React, {Fragment} from "react";
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
+//RUTAS
+
+import Home  from '../views/Home/home';   //SE IMPORTA HOME.JS DE VISTAS
+import userSignin from '../views/users/signin/signin'
+
 function App() {
   return (
     <Fragment>
+      <Router>
 
+      <Route path= "/" element={<Home />}> </ Route>
+      <Route path= "/user" element = {<userSignin/>}> </ Route>
+
+      </Router>
       
         
       
