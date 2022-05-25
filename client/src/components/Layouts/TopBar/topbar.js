2670bbb062556b1aa10d9f8dadd22b0806f4dd8b
Ragnalex <66427660+Ragnalex@users.noreply.github.com>
2022-05-25 13:38:26 -0400

    Topbar con solo background

---
+++ client/src/components/Layouts/TopBar/topbar.js
@@ -0,0 +1,16 @@
+
+import "./topbar.css";
+
+const topbar = () => {      //SE LLAMA AL ESTILO topback de TOPBAR
+    return (
+        <div className="container mb-3">
+            <div className="topback">      
+                
+            </div>
+        </div>
+        
+    );
+}
+
+
+export default topbar;
