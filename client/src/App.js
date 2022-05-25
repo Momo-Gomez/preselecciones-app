2240e477545a33f5758cb2b6c2511c62bb2b1ec2
Ragnalex <66427660+Ragnalex@users.noreply.github.com>
2022-05-25 13:39:04 -0400

    Topbar BG

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,10 @@ import './App.css';
 import React, {Fragment} from "react";
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
+//LAYOUTS
+import Header from './components/Layouts/Header/header'   //SE IMPORTA EL HEADER FIJO
+import Topbar from './components/Layouts/TopBar/topbar'   //SE IMPORTA LA TOPBAR FIJA
+
 //RUTAS
 
 import Home  from './views/Home/home';   //SE IMPORTA HOME.JS DE VISTAS
@@ -10,12 +14,13 @@ import Home  from './views/Home/home';   //SE IMPORTA HOME.JS DE VISTAS
 function App() {
   return (
     <Fragment>
-  
       <Router>
+        <Topbar />
         <Routes>
 
-          <Route path= "/" element={<Home />}> </ Route>
+          <Route path= "/" element={<Home />}> </ Route>        {/* LLAMANDO A LA RUTA / DESDE EL HOME.JS */}
           
+
         </Routes>
 
       </Router>
