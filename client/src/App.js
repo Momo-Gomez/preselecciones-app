7b5967e3ac7dde46c9920c97b0596922c15f153f
Ragnalex <66427660+Ragnalex@users.noreply.github.com>
2022-05-25 12:08:05 -0400

    Carpeta components

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,11 @@
 import './App.css';
 import React, {Fragment} from "react";
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <Fragment>
-      <div>
-        <div className='container '>
-          <div>
-            <div className='home-background border border-dark border-opacity-25'>
-              <div className='btn-group d-flex flex-row-reverse'>
-                  <div className='mr-3'>
-                    asfasfgasgfas
-                  </div>
-                  <div className='ml-3'>
-                    asdafasda
-                  </div>
-                
-                
-              </div>
-                
-            </div>
-            
-            <div className="background-search mb-3 border border-dark border-opacity-25">
-                    
-                    <h1 className="text-center mb-3"> Busca aqui tu beneficio </h1>
-                    <div className='mb-3'>
-                      probando
-                    </div>
-                
-                  
-            </div>
-          </div>
-        </div>
-      </div>
+
       
         
       
