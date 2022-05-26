8b81a17909097f68bca8fad535ccb56c1de1925e
Conejo <vf.araya.a@gmail.com>
2022-05-25 23:39:11 -0400

    Labels terminados sin useState

--- client/src/views/users/signup/signup.js
+++ client/src/views/users/signup/signup.js
@@ -6,8 +6,48 @@ export default function signup(){
             <span className="signup-title">Registro</span>
             <form className="signup-form">
                 
+                {/* Información solicitada al cliente */}
                 <label>Nombre</label>
+                <input 
+                    type="text"
+                    className="signup-input"
+                    placeholder="Nombre"
+                />
+                
+                <label>Apellidos</label>
+                <input
+                    type="text"
+                    className="signup-input"
+                    placeholder="Apellidos"
+                />
 
+                <label>RUT</label>
+                <input
+                    type="text"
+                    className="signup-input"
+                    placeholder="RUT"
+                />
+
+                <label>Email</label>
+                <input
+                    type="text"
+                    className="signup-input"
+                    placeholder="Email"
+                />
+
+                <label>Contraseña</label>
+                <input
+                    type="text"
+                    className="signup-input"
+                    placeholder="****"
+                />
+
+                <label>Revalidar contraseña</label>
+                <input
+                    type="text"
+                    className="signup-input"
+                    placeholder="****"
+                />
             </form>
 
         </div>
