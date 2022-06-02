9d28da67180c64858650cb8b6dadada008d8c8f2
Miguel Gomez <miguel.gomez@alumnos.ucn.cl>
2022-06-02 14:22:16 -0400

    formateado y eslinc server

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,37 @@
-import './App.css';
-import React, {Fragment} from "react";
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import "./App.css";
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 //LAYOUTS
-import Header from './components/Layouts/Header/header'   //SE IMPORTA EL HEADER FIJO
-import Topbar from './components/Layouts/TopBar/topbar'   //SE IMPORTA LA TOPBAR FIJA
+import Header from "./components/Layouts/Header/header"; //SE IMPORTA EL HEADER FIJO
+import Topbar from "./components/Layouts/TopBar/topbar"; //SE IMPORTA LA TOPBAR FIJA
 
 //RUTAS
 
-import Home  from './views/Home/home';   //SE IMPORTA HOME.JS DE VISTAS
-import Signin from './views/users/signin/signin' //SE IMPORTA SIGNIN.JS DE VISTAS
-
+import Home from "./views/Home/home"; //SE IMPORTA HOME.JS DE VISTAS
+import Signin from "./views/users/signin/signin"; //SE IMPORTA SIGNIN.JS DE VISTAS
 
 function App() {
   return (
     <Fragment>
-      <div className='container'>
+      <div className="container">
         <Router>
           <div>
             <Topbar />
           </div>
-          
+
           <Routes>
-
-            <Route path= "/" element={<Home />}> </ Route>        {/* LLAMANDO A LA RUTA / DESDE EL HOME.JS */}
-            <Route path= "/usuario/login" element = { <Signin /> }> </Route>    {/*LLAMANDO A LA RUTA USUARIO/LOGIN DESDE EL SIGNIN.JS */}
-            
-
+            <Route path="/" element={<Home />}>
+              {" "}
+            </Route>{" "}
+            {/* LLAMANDO A LA RUTA / DESDE EL HOME.JS */}
+            <Route path="/usuario/login" element={<Signin />}>
+              {" "}
+            </Route>{" "}
+            {/*LLAMANDO A LA RUTA USUARIO/LOGIN DESDE EL SIGNIN.JS */}
           </Routes>
-
         </Router>
       </div>
-      
-      
-        
-      
     </Fragment>
   );
 }
--- client/src/components/Layouts/TopBar/topbar.js
+++ client/src/components/Layouts/TopBar/topbar.js
@@ -1,36 +1,44 @@
-
 import "./topbar.css";
 
-const topbar = () => {     
-    return (
-        <div className="container">
-            <div className="topback">      {/*SE LLAMA AL ESTILO topback de TOPBAR*/}
-                    <div className="container d-flex justify-content-end">
-                    <button type="button" className="btn btn-primary 
+const topbar = () => {
+  return (
+    <div className="container">
+      <div className="topback">
+        {" "}
+        {/*SE LLAMA AL ESTILO topback de TOPBAR*/}
+        <div className="container d-flex justify-content-end">
+          <button
+            type="button"
+            className="btn btn-primary 
                                                     border border-white border-opacity-25
                                                     btn-group
                                                     mt-3 me-3
                                                     shadow rounded-pill"
-                                                    aria-label = "First group"> {/*Parametros de diseño en bootstrap para el boton */}
-                    Ingresar 
-                    </button>
+            aria-label="First group"
+          >
+            {" "}
+            {/*Parámetros de diseño en bootstrap para el boton */}
+            Ingresar
+          </button>
 
-                    <button type="button" className="btn btn-danger
+          <button
+            type="button"
+            className="btn btn-danger
                                                     border border-white border-opacity-25
                                                     btn-group
                                                     mt-3 me-3
                                                     shadow
                                                     rounded-pill"
-                                                    aria-label = "Second group"> {/*Parametros de diseño en bootstrap para el boton */}
-                    Registro </button>
-                    </div>
-                
-                
-            </div>
+            aria-label="Second group"
+          >
+            {" "}
+            {/*Parámetros de diseño en bootstrap para el botón */}
+            Registro{" "}
+          </button>
         </div>
-        
-    );
-}
-
+      </div>
+    </div>
+  );
+};
 
 export default topbar;
