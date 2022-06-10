ee9f1b36c0ceb0bfbd8c99d622edd90d653fd17e
luciano-tapia <74554288+luciano-tapia@users.noreply.github.com>
2022-06-09 20:05:11 -0400

    SUBSIDIOS

--- client/src/views/Subsidios/subsidio.js
+++ client/src/views/Subsidios/subsidio.js
@@ -1,5 +1,5 @@
-
-
+import React, { Fragment } from "react";
+import "./subsidio.css";
 
 
 const subsidio = () => {
@@ -7,42 +7,52 @@ const subsidio = () => {
         <div className="container">
             <h1 className="text-center text-bg-danger p-3 "></h1>
             <h2 className="text-primary  p-3 ">Lista de Beneficios</h2>
-            <div class="card mb-3">
-                <h5 class="card-header text-bg-primary ">DS-27 Reparaciones de todo tipo</h5>
-                <div class="card-body">
-                    <p class="card-text ">Aqui te dan re muchas custiones</p>
-                    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                        <button class="btn btn-primary me-md-2" type="button">+ Info</button>
+
+            <div className="CartaBusqueda">
+                <div class="card mb-5">
+                    <h5 class="card-header text-bg-primary ">DS-27 Reparaciones de todo tipo</h5>
+                    <div class="card-body">
+                        <p class="card-text ">Aqui te dan re muchas custiones</p>
+                        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                            <button class="btn btn-primary me-md-2" type="button">+ Info</button>
+                        </div>
+                    </div>
+                </div> 
+                
+            </div>  
+            <div className="CartaBusqueda">
+                <div class="card mb-5">
+                    <h5 class="card-header text-bg-primary">DS-10 Viviendas Rurales</h5>
+                    <div class="card-body">
+                        <p class="card-text">Aqui te dan re muchas custiones</p>
+                        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                            <button class="btn btn-primary me-md-2" type="button">+ Info</button>
+                        </div>
                     </div>
                 </div>
-            </div> 
-            <div class="card mb-3">
-                <h5 class="card-header text-bg-primary">DS-10 Viviendas Rurales</h5>
-                <div class="card-body">
-                    <p class="card-text">Aqui te dan re muchas custiones</p>
-                    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                        <button class="btn btn-primary me-md-2" type="button">+ Info</button>
+            </div>    
+            <div className="CartaBusqueda">
+                <div class="card mb-5">
+                    <h5 class="card-header text-bg-primary">DS-01 Vivienda rural (Sitio propio)</h5>
+                    <div class="card-body">
+                        <p class="card-text">Aqui te dan re muchas custiones</p>
+                        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                            <button class="btn btn-primary me-md-2" type="button">+ Info</button>
+                        </div>
                     </div>
-                </div>
-            </div> 
-            <div class="card mb-3">
-                <h5 class="card-header text-bg-primary">DS-01 Vivienda rural (Sitio propio)</h5>
-                <div class="card-body">
-                    <p class="card-text">Aqui te dan re muchas custiones</p>
-                    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                        <button class="btn btn-primary me-md-2" type="button">+ Info</button>
+                </div> 
+            </div>
+            <div className="CartaBusqueda">
+                <div class="card mb-5">
+                    <h5 class="card-header text-bg-primary">DS-49 Vivienda Espacio urbano</h5>
+                    <div class="card-body">
+                        <p class="card-text">Aqui te dan re muchas custiones</p>
+                        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                            <button class="btn btn-primary me-md-2" type="button">+ Info</button>
+                        </div>
                     </div>
-                </div>
-            </div> 
-            <div class="card mb-3">
-                <h5 class="card-header text-bg-primary">DS-49 Vivienda Espacio urbano</h5>
-                <div class="card-body">
-                    <p class="card-text">Aqui te dan re muchas custiones</p>
-                    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                        <button class="btn btn-primary me-md-2" type="button">+ Info</button>
-                    </div>
-                </div>
-            </div> 
+                </div> 
+            </div>    
         </div>   
     
     );
