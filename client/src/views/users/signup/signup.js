62ace44eb3b10077cb2346a52158ef96c735109b
Conejo <vf.araya.a@gmail.com>
2022-06-12 21:40:57 -0400

    Cambios en pag de registro
    Se agregó:
        > tarjeta
        > imagen de fondo
        > forma de labels

--- client/src/views/users/signup/signup.js
+++ client/src/views/users/signup/signup.js
@@ -4,47 +4,54 @@ export default function signup() {
   return (
     <div className="container">
       <div className="signup">
-        <span className="signup-title text-center">Registro</span>
-        <form className="signup-form">
-          {/* Información solicitada al cliente */}
-          <label>Nombre</label>
-          <input type="text" className="form-control" placeholder="Nombre" />
+        {/*  Tarjeta  */}
+        <div className="card mt-3">
+          <span className="signup-title text-center">Registro</span>
+          <form className="signup-form">
+            {/* Información solicitada al cliente */}
+            <label>Nombre</label>
+            <input type="text" className="form-control" placeholder="Nombre" />
 
-          <label>Apellidos</label>
-          <input type="text" className="form-control" placeholder="Apellidos" />
+            <label>Apellidos</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Apellidos"
+            />
 
-          <label>RUT</label>
-          <input type="text" className="form-control" placeholder="RUT" />
+            <label>RUT</label>
+            <input type="text" className="form-control" placeholder="RUT" />
 
-          <label>Email</label>
-          <input type="text" className="form-control" placeholder="Email" />
+            <label>Email</label>
+            <input type="text" className="form-control" placeholder="Email" />
 
-          <label>Contraseña</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Contraseña"
-          />
+            <label>Contraseña</label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Contraseña"
+            />
 
-          <label>Revalidar contraseña</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Contraseña"
-          />
-        </form>
-        <button
-          type="submit"
-          className="submit-button btn btn-primary
+            <label>Revalidar contraseña</label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Contraseña"
+            />
+          </form>
+          <button
+            type="submit"
+            className="submit-button btn btn-primary
                                             border border-white border-opacity-25
                                             btn-gorup
                                             mt-4 me-4
                                             shadow rounded-pill"
-        >
-          {" "}
-          {/*Parámetros de diseño en bootstrap para el boton */}
-          Enviar
-        </button>
+          >
+            {" "}
+            {/*Parámetros de diseño en bootstrap para el boton */}
+            Enviar
+          </button>
+        </div>
       </div>
     </div>
   );
