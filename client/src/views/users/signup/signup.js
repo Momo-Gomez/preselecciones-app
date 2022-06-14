50c8493b0a3b2e8e26a6b6d88ec2f7574ed4ea11
Conejo <vf.araya.a@gmail.com>
2022-06-13 23:31:49 -0400

    Animaciones de inputs
    o al menos un intento de ello

--- client/src/views/users/signup/signup.js
+++ client/src/views/users/signup/signup.js
@@ -9,21 +9,25 @@ export default function signup() {
           <span className="signup-title text-center">Registro</span>
           <form className="signup-form">
             {/* Información solicitada al cliente */}
-            {/*---*/}
-            <label>Primer Nombre</label>
+            {/*--- PRIMER NOMBRE ---*/}
+            <label for="f-name">Primer Nombre</label>
             <input 
+              id="f-name"
               type="text" pattern="[A-Za-z]*"
-              className="form-control" 
-              placeholder="Mario" />
+              className="styl"
+              placeholder="Alex"
+            />
 
-            {/*---*/}
-            <label>Segundo Nombre</label>
+            {/*--- SEGUNDO NOMBRE ---*/}
+            <label for= "s-name">Segundo Nombre</label>
             <input 
+              id="s-name"
               type="text" pattern="[A-Za-z]*"
-              className="form-control" 
-              placeholder="Luís" />
+              className="form-control"
+              placeholder="Cruz"
+            />
 
-            {/*---*/}
+            {/*--- APELLIDO PATERNO ---*/}
             <label>Apellido Paterno</label>
             <input
               type="text" pattern="[A-Za-z]*"
@@ -31,57 +35,61 @@ export default function signup() {
               placeholder="Pérez"
             />
 
-            {/*---*/}
+            {/*--- APELLIDO MATERNO ---*/}
             <label>Apellido Materno</label>
             <input
               type="text" pattern="[A-Za-z]*"
               className="form-control"
-              placeholder="Gonzáles"
+              placeholder="Pérez"
             />
 
-            {/*---*/}
+            {/*--- RUT ---*/}
             <label>RUT</label>
             <div class="input-group">
               <input
-                type="number" pattern="[0-9]*"
+                type="text" pattern="[0-9]*"
                 className="form-control"
                 placeholder="12345678"
-              />
+               />
               <span class="imput-group-addon">-</span>
               <div class="digit">
                 <input 
-                  type="number" pattern="[0-9]{1}"
+                  type="char" pattern="[k0-9]{1}"
                   className="form-control" 
                   placeholder="9"
                 />
-              </div>
+               </div>
             </div>
 
-            {/*---*/}
+            {/*--- EMAIL ---*/}
             <label>Email</label>
             <input
-              type="email"
-              pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
-              className="form-control"
-              placeholder="soy_mejor_que_tu@ejemplo.com"
-            />
+             type="email"
+             pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+             className="form-control"
+             placeholder="ejemplo@dominio.com"
+             />
 
-            {/*---*/}
+            {/*--- CONTRASEÑA ---*/}
             <label>Contraseña</label>
             <input
               type="password"
-              pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,10}"
+              pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z])"
+              min="4"
+              max="10"
               className="form-control"
-              placeholder="****"
+              placeholder="*****"
             />
 
-            {/*---*/}
+            {/*--- REVALIDACIÓN ---*/}
             <label>Revalidar contraseña</label>
             <input
               type="password"
-              pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,}"
+              pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z])"
+              min="4"
+              max="10"
               className="form-control"
-              placeholder="****"
+              placeholder="*****"
             />
           </form>
           <div className="text-left">
