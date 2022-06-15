f908072e8e2d4ee6eadd13864463cb98217baebc
luciano-tapia <74554288+luciano-tapia@users.noreply.github.com>
2022-06-15 09:30:59 -0400

    Update subsidio.js

--- client/src/views/Subsidios/subsidio.js
+++ client/src/views/Subsidios/subsidio.js
@@ -4,55 +4,56 @@ import "./subsidio.css";
 
 const subsidio = () => {
     return (
+        
         <div className="container">
             <h1 className="text-center text-bg-danger p-3 "></h1>
             <h2 className="text-primary  p-3 ">Lista de Beneficios</h2>
 
-            <div className="CartaBusqueda">
-                <div class="card mb-5">
-                    <h5 class="card-header text-bg-primary ">DS-27 Reparaciones de todo tipo</h5>
-                    <div class="card-body">
-                        <p class="card-text ">Aqui te dan re muchas custiones</p>
-                        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                            <button class="btn btn-primary me-md-2" type="button">+ Info</button>
-                        </div>
-                    </div>
-                </div> 
-                
-            </div>  
-            <div className="CartaBusqueda">
-                <div class="card mb-5">
-                    <h5 class="card-header text-bg-primary">DS-10 Viviendas Rurales</h5>
-                    <div class="card-body">
-                        <p class="card-text">Aqui te dan re muchas custiones</p>
-                        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                            <button class="btn btn-primary me-md-2" type="button">+ Info</button>
-                        </div>
+            
+            <div class="card mb-5 shadow p-3 bg-body rounded">
+                <h5 class="card-header text-bg-primary ">DS-27 Reparaciones de todo tipo</h5>
+                <div class="card-body">
+                    <h5 class="card-title">Mejoramiento de la vivienda</h5>
+                    <p class="card-text ">Obras de carácter estructural de instalaciones (sanitarias, eléctricas o gas), reparación de la envolvente (techumbre), y de mantención (terminaciones, revestimientos, ventanas, puertas, pinturas, cierros, entre otros).</p>
+                    <h5 class="card-title">Ampliación de la vivienda</h5>
+                    <p class="card-text ">Obras destinadas aumentar la superficie de la vivienda existente para resolver problemas sanitarios o de hacinamiento. Por ejemplo, construcción de un nuevo baño, dormitorio o cocina.</p>
+                    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                        <button class="btn btn-primary me-md-2" type="button">+ Info</button>
                     </div>
                 </div>
-            </div>    
-            <div className="CartaBusqueda">
-                <div class="card mb-5">
-                    <h5 class="card-header text-bg-primary">DS-01 Vivienda rural (Sitio propio)</h5>
-                    <div class="card-body">
-                        <p class="card-text">Aqui te dan re muchas custiones</p>
-                        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                            <button class="btn btn-primary me-md-2" type="button">+ Info</button>
-                        </div>
+            </div> 
+             
+            
+        
+            <div class="card mb-5 shadow p-3 bg-body rounded">
+                <h5 class="card-header text-bg-primary">DS-10 Viviendas Rurales</h5>
+                <div class="card-body">
+                    <p class="card-text">Este subsidio del Estado permite construir una vivienda en el terreno donde vive la familia interesada y se puede postular de manera individual o colectiva.</p>
+                    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                        <button class="btn btn-primary me-md-2" type="button">+ Info</button>
                     </div>
-                </div> 
+                </div>
             </div>
-            <div className="CartaBusqueda">
-                <div class="card mb-5">
-                    <h5 class="card-header text-bg-primary">DS-49 Vivienda Espacio urbano</h5>
-                    <div class="card-body">
-                        <p class="card-text">Aqui te dan re muchas custiones</p>
-                        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                            <button class="btn btn-primary me-md-2" type="button">+ Info</button>
-                        </div>
+               
+            <div class="card mb-5 shadow p-3 bg-body rounded">
+                <h5 class="card-header text-bg-primary">DS-01 Vivienda rural (Sitio propio)</h5>
+                <div class="card-body">
+                    <p class="card-text">Aqui te dan re muchas custiones</p>
+                    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                        <button class="btn btn-primary me-md-2" type="button">+ Info</button>
                     </div>
-                </div> 
-            </div>    
+                </div>
+            </div> 
+            
+            <div class="card mb-5 shadow p-3 bg-body rounded">
+                <h5 class="card-header text-bg-primary">DS-49 Vivienda Espacio urbano</h5>
+                <div class="card-body">
+                    <p class="card-text">Este apoyo del Estado permite construir una vivienda o un conjunto de ellas (casas o departamentos) sin crédito hipotecario en sectores urbanos o rurales</p>
+                    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                        <button class="btn btn-primary me-md-2" type="button">+ Info</button>
+                    </div>
+                </div>
+            </div>   
         </div>   
     
     );
