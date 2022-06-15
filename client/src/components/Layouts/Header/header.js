6d3c90854088b14e42b21e752a3548e988806c27
Conejo <vf.araya.a@gmail.com>
2022-06-15 18:11:21 -0400

    Cambios Header y search bar
    > Paleta de colores
    > Diseño

---
+++ client/src/components/Layouts/Header/header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './header.css'
+import CP from '../../../assets/familiaCasa.jpg'
+
+const Header = () => {
+    return (
+        <header>
+            <div className= 'container header-container'>    
+                    <div class= "couple d-flex justify-content-center">
+                        <img src={CP} alt="couple" />
+                    </div>
+                </div>
+        
+        </header>
+    )
+}
+
+export default Header;
--- client/src/views/Home/home.js
+++ client/src/views/Home/home.js
@@ -1,40 +1,14 @@
 import React, { Fragment } from "react";
-import Header from "../../components/Layouts/Header/header";
+import Header from '../../components/Layouts/Header/header';
+import SearchBar from "./SearchBar/search-bar";
 import "./home.css";
 
 const home = () => {
   return (
-    <div className="container">
+    <div className="container home-content">
       <div className="text-center">
         <Header/>
-        <div className="SearchBarBG">
-          <div className="container">
-            <h1 className="fw-semibold"> Busca Aquí tu Beneficio </h1>
-            <div className="row justify-content-center">
-              <div className="col-5 d-flex justify-content-center">
-                <div className="input-group">
-                  {" "}
-                  {/*Llamado al input para despliegue de lista */}
-                  <select
-                    className="form-select"
-                    aria-label="SubsidiosDespliegue"
-                  >
-                    <option selected>Buscar...</option>
-                    <option value="1">DS-27 Reparaciones de todo tipo</option>
-                    <option value="2">DS-10 Viviendas Rurales</option>
-                    <option value="3">
-                      DS-01 Vivienda rural (Sitio propio)
-                    </option>
-                    <option value="4">DS-49 Vivienda Espacio urbano</option>
-                  </select>
-                  <button class="btn btn-primary" type="button">
-                    Buscar
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <SearchBar/>
       </div>
     </div>
   );
