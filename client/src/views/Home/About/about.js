2d9c40891836f5d4dea8871f6bba5d6776b78aa5
Conejo <vf.araya.a@gmail.com>
2022-06-15 21:08:55 -0400

    intento de arreglo de about

--- client/src/views/Home/About/about.js
+++ client/src/views/Home/About/about.js
@@ -1,43 +1,58 @@
 import React from "react";
-import './about.css'
+import "./about.css";
 
-import LIST from '../../../assets/about.jpg'
-import FORM from '../../../assets/about.jpg'
+import LIST from "../../../assets/about.jpg";
+import FORM from "../../../assets/about.jpg";
 
 const About = () => {
-    return (
-        <div class= 'container'>
-            <div className='about-tittle'>
-                <h2>Haz tus postulaciones de manera sencilla</h2>
-                <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis id mi eget vestibulum. Suspendisse id mauris quis sapien aliquet gravida.</h5>
+  return (
+    <div class="container">
+      <section id="about">
+        <h2>Haz tus postulaciones de manera sencilla</h2>
+        <h5>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis
+          id mi eget vestibulum. Suspendisse id mauris quis sapien aliquet
+          gravida.
+        </h5>
+
+        <section>
+          <div className="container about__container">
+            <div className="about__list">
+              <div className="about__list-img">
+                <img src={LIST} alt="List About" />
+              </div>
             </div>
 
-            <section>
-                <div className="container about__container">
-                    <h2>Infórmate</h2>
-                    <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis id mi eget vestibulum. Suspendisse id mauris quis sapien aliquet gravida.</h5>
-                    <div className="about__list">
-                    <div className="about__list-img">
-                        <img src={LIST} alt="List About"/>
-                    </div>
-                    </div>
-                </div>
-            </section>
+            <div className="about__content">
+              <h2>Infórmate</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+                convallis id mi eget vestibulum. Suspendisse id mauris quis
+                sapien aliquet gravida.
+              </p>
+            </div>
 
-            <section>
-                <div className="container about__container">
-                <h2>Sube</h2>
-                <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis id mi eget vestibulum. Suspendisse id mauris quis sapien aliquet gravida.</h5>
-                <div className="about__list">
-                    <div className="about__list-img">
-                    <img src={FORM} alt="List About"/>
-                    </div>
+            <div className="container about__container">
+              <div className="about__list">
+                <div className="about__list-img">
+                  <img src={FORM} alt="List About" />
                 </div>
-                </div>
-            </section>
-            
-        </div>
-    )
-}
+              </div>
+            </div>
+
+            <div className="about__content">
+              <h2>Sube</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+                convallis id mi eget vestibulum. Suspendisse id mauris quis
+                sapien aliquet gravida.
+              </p>
+            </div>
+          </div>
+        </section>
+      </section>
+    </div>
+  );
+};
 
 export default About;
