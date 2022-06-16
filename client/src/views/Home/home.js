553a4e21b70749e51a18a44b3142a8d54ced6edc
Conejo <vf.araya.a@gmail.com>
2022-06-15 20:08:06 -0400

    añadiendo about

--- client/src/views/Home/home.js
+++ client/src/views/Home/home.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from "react";
 import Header from '../../components/Layouts/Header/header';
 import SearchBar from "./SearchBar/search-bar";
+import About from "./About/about";
 import "./home.css";
 
 const home = () => {
@@ -9,6 +10,7 @@ const home = () => {
       <div className="text-center">
         <Header/>
         <SearchBar/>
+        <About/>
       </div>
     </div>
   );
