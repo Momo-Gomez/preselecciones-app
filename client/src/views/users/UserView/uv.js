584dc71d21440bcf5ec32518a0213fa0bcbb7d1e
Conejo <vf.araya.a@gmail.com>
2022-07-16 16:09:18 -0400

    base de vista usuario

--- client/src/views/users/UserView/uv.js
+++ client/src/views/users/UserView/uv.js
@@ -9,7 +9,20 @@ export default function UserView()  {
             <h1 class="text-center p-5"></h1>
             
             <div class="profile-card card mt-3">
+                {/* Nombre del usuario */}
+                <span class="name">Nombre</span>
+                <label class="RUT">12.345.678-9</label>
+                <label class="email">emailname@domain.com</label>
+            </div>
 
+            <div class="bft-container">
+                <form class="bft-form">
+                    <span class="btf-title">Mis Beneficios</span>
+
+                    <div class="bft-group">
+                        
+                    </div>
+                </form>
             </div>
         </div>
     )
