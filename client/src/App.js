b2e9d9086c00b50f7b5fc1603a306f0f77580224
Miguel Gomez <miguel.gomez@alumnos.ucn.cl>
2022-07-20 14:54:27 -0400

    context view

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import React, { Fragment } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router,Routes, Switch, Route, Link } from "react-router-dom";
+import { useContext } from "react";
+import { Context } from "./context/Context";
 
 //LAYOUTS
 import Header from "./components/Layouts/Header/header"; //SE IMPORTA EL HEADER FIJO
@@ -11,11 +13,12 @@ import Topbar from "./components/Layouts/TopBar/topbar"; //SE IMPORTA LA TOPBAR 
 import Home from "./views/Home/home"; //SE IMPORTA HOME.JS
 import Signin from "./views/users/signin/signin"; //SE IMPORTA SIGNIN.JS
 import Signup from "./views/users/signup/signup"; //SE IMPORTA SIGNUP.JS
-import Subsidio from "./views/Subsidios/subsidio"; //SE IMPORTA SUBSIDIO.JS
+import Subsidies from "./views/Subsidios/subsidio"; //SE IMPORTA SUBSIDIO.JS
 import Form from "./views/Formulario/form"; //SE IMPORTA FORM.JS
 import User from "./views/users/UserView/uv"; //SE IMPORTA UV.JS
 
 function App() {
+  const { user } = useContext(Context);
   return (
     <Fragment>
       <div className="container">
@@ -29,23 +32,23 @@ function App() {
               {" "}
             </Route>{" "}
             {/* LLAMANDO A LA RUTA / DESDE EL HOME.JS */}
-            <Route path="/usuario/ingreso" element={<Signin />}>
+            <Route path="/usuario/ingreso" element={user ? < User/> : <Signin />}>
               {" "}
             {/*LLAMANDO A LA RUTA USUARIO/LOGIN DESDE EL SIGNIN.JS */}
             </Route>{" "}
-            <Route path="/usuario/registro" element={<Signup />}>
+            <Route path="/usuario/registro" element={user ? < User/> : <Signup  />}>
               {" "}
             {/*LLAMANDO A LA RUTA USUARIO/SIGNUP DESDE EL SIGNUP.JS */}
             </Route>
-            <Route path="/usuario" element={<User />}>
+            <Route path="/usuario" element={user ? < User/> : <Signup  />}>
               {" "}
             {/*LLAMANDO A LA RUTA SUSUARIO DESDE EL UV.JS */}
             </Route>{" "}
-            <Route path= "/subsidios" element={<Subsidio />}>
+            <Route path= "/subsidios" element={<Subsidies/>}>
               {" "}
             {/*LLAMANDO A LA RUTA Subsidios/subsidio DESDE EL SUBSIDIO.JS */}
             </Route>{" "}
-            <Route path="/subsidios/formulario" element={<Form />}>
+            <Route path="/usuario/formulario" element={user ? < Form/> : <Signin />}>
               {" "}
             {/*LLAMANDO A LA RUTA SUBSIDIOS/FORMULARIO DESDE EL FORMULARIO.JS */}
             </Route>{" "}
