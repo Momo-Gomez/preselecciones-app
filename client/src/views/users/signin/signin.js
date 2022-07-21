862201cd12afd41e69c32c34e4281d4defb4c261
Miguel Gomez <miguel.gomez@alumnos.ucn.cl>
2022-07-21 13:38:37 -0400

    cambio de rutas back

--- client/src/views/users/signin/signin.js
+++ client/src/views/users/signin/signin.js
@@ -20,6 +20,7 @@ export default function Login() {
     e.preventDefault();
     dispatch( { type: "LOGIN_START" } );
     try {
+      
       const res = await axios.post("http://localhost:5000/api/auth/usuario/ingreso",{
         rut: rutRef.current.value,
         contrasena: passRef.current.value,
