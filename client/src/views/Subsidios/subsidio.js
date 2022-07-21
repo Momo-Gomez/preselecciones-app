1f9c4fd36aae00cc309cad87d8dce53faa728986
Conejo <vf.araya.a@gmail.com>
2022-07-21 18:33:02 -0400

    Tarjetas de usuario

--- client/src/views/Subsidios/subsidio.js
+++ client/src/views/Subsidios/subsidio.js
@@ -2,63 +2,79 @@ import React, { Fragment } from "react";
 import "./subsidio.css";
 import { useNavigate } from "react-router-dom";
 
-
 const subsidio = () => {
-    const navigate = useNavigate();
-    return (
-        
-        <div className="s-container">
-            <h1 className="text-center p-3 "></h1>
-            <h3 class="text-center p-3"></h3>
-            <h2 className="p-5 ">Lista de Beneficios</h2>
+  const navigate = useNavigate();
+  return (
+    <div className="s-container">
+      <h1 className="text-center p-3 "></h1>
+      <h3 class="text-center p-3"></h3>
 
-            
-            <div class="card mb-5 shadow bg-body rounded">
-                <h5 class="card-header">DS-27 Reparaciones de todo tipo</h5>
-                <div class="card-body">
-                    <h5 class="card-title">Mejoramiento de la vivienda</h5>
-                    <p class="card-text ">Obras de carácter estructural de instalaciones (sanitarias, eléctricas o gas), reparación de la envolvente (techumbre), y de mantención (terminaciones, revestimientos, ventanas, puertas, pinturas, cierros, entre otros).</p>
-                    <h5 class="card-title">Ampliación de la vivienda</h5>
-                    <p class="card-text ">Obras destinadas aumentar la superficie de la vivienda existente para resolver problemas sanitarios o de hacinamiento. Por ejemplo, construcción de un nuevo baño, dormitorio o cocina.</p>
-                    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                        <button onClick={() => navigate('/usuario/formulario')} class="form-btn btn btn-primary me-md-2" type="button">Hacer Trámite</button>
-                    </div>
-                </div>
-            </div> 
-             
-            
-        
-            <div class="card mb-5 shadow bg-body rounded">
-                <h5 class="card-header">DS-10 Viviendas Rurales</h5>
-                <div class="card-body">
-                    <p class="card-text">Este subsidio del Estado permite construir una vivienda en el terreno donde vive la familia interesada y se puede postular de manera individual o colectiva.</p>
-                    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                        <button onClick={() => navigate('/usuario/formulario')} class="form-btn btn btn-primary me-md-2" type="button">Hacer Trámite</button>
-                    </div>
-                </div>
-            </div>
-               
-            <div class="card mb-5 shadow bg-body rounded">
-                <h5 class="card-header">DS-01 Vivienda rural (Sitio propio)</h5>
-                <div class="card-body">
-                    <p class="card-text">Este apoyo del Estado permite a las familias que no son dueñas de una vivienda y tienen capacidad de ahorro, construir una vivienda de hasta 140 m2 en un sitio propio o en un terreno donde ya existe otra vivienda.</p>
-                    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                        <button onClick={() => navigate('/usuario/formulario')} class="form-btn btn btn-primary me-md-2" type="button">Hacer Trámite</button>
-                    </div>
-                </div>
-            </div> 
-            
-            <div class="card mb-5 shadow bg-body rounded">
-                <h5 class="card-header">DS-49 Vivienda Espacio urbano</h5>
-                <div class="card-body">
-                    <p class="card-text">Este apoyo del Estado permite construir una vivienda o un conjunto de ellas (casas o departamentos) sin crédito hipotecario en sectores urbanos o rurales.</p>
-                    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                        <button onClick={() => navigate('/usuario/formulario')} class="form-btn btn btn-primary me-md-2" type="button">Hacer Trámite</button>
-                    </div>
-                </div>
-            </div>   
-        </div>   
-    
-    );
-    
-};export default subsidio;
+      <div class="header-cont">
+      <h2 className="p-5 ">Lista de Beneficios</h2>
+      <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+        <button
+          onClick={() => navigate("/usuario/formulario")}
+          class="form-btn btn btn-primary me-md-2"
+          type="button"
+        >
+          Hacer Trámite
+        </button>
+      </div>
+      </div>
+
+      <div class="card mb-5 shadow bg-body rounded">
+        <h5 class="card-header">DS-27 Reparaciones de todo tipo</h5>
+        <div class="card-body">
+          <h5 class="card-title">Mejoramiento de la vivienda</h5>
+          <p class="card-text ">
+            Obras de carácter estructural de instalaciones (sanitarias,
+            eléctricas o gas), reparación de la envolvente (techumbre), y de
+            mantención (terminaciones, revestimientos, ventanas, puertas,
+            pinturas, cierros, entre otros).
+          </p>
+          <h5 class="card-title">Ampliación de la vivienda</h5>
+          <p class="card-text ">
+            Obras destinadas aumentar la superficie de la vivienda existente
+            para resolver problemas sanitarios o de hacinamiento. Por ejemplo,
+            construcción de un nuevo baño, dormitorio o cocina.
+          </p>
+        </div>
+      </div>
+
+      <div class="card mb-5 shadow bg-body rounded">
+        <h5 class="card-header">DS-10 Viviendas Rurales</h5>
+        <div class="card-body">
+          <p class="card-text">
+            Este subsidio del Estado permite construir una vivienda en el
+            terreno donde vive la familia interesada y se puede postular de
+            manera individual o colectiva.
+          </p>
+        </div>
+      </div>
+
+      <div class="card mb-5 shadow bg-body rounded">
+        <h5 class="card-header">DS-01 Vivienda rural (Sitio propio)</h5>
+        <div class="card-body">
+          <p class="card-text">
+            Este apoyo del Estado permite a las familias que no son dueñas de
+            una vivienda y tienen capacidad de ahorro, construir una vivienda de
+            hasta 140 m2 en un sitio propio o en un terreno donde ya existe otra
+            vivienda.
+          </p>
+        </div>
+      </div>
+
+      <div class="card mb-5 shadow bg-body rounded">
+        <h5 class="card-header">DS-49 Vivienda Espacio urbano</h5>
+        <div class="card-body">
+          <p class="card-text">
+            Este apoyo del Estado permite construir una vivienda o un conjunto
+            de ellas (casas o departamentos) sin crédito hipotecario en sectores
+            urbanos o rurales.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default subsidio;
--- client/src/views/users/UserView/uv.js
+++ client/src/views/users/UserView/uv.js
@@ -1,34 +1,82 @@
-import './uv.css'
-import React from 'react'
+import "./uv.css";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { Context } from "../../../context/Context";
 
-export default function UserView()  {
-    const navigate = useNavigate();
-    const { user } = useContext(Context);// para mostrar los datos del usuario
-    return (
-        <div className="uv-container">
-            <h1 class="text-center p-5"></h1>
+export default function UserView() {
+  const navigate = useNavigate();
+  const { user } = useContext(Context); // para mostrar los datos del usuario
+  return (
+    <div className="uv-container">
+      <h1 class="text-center p-5"></h1>
 
-            <div class="uv-card card m-3">
-                <span class="uv-name">Bienvenido {user.pnombre} {user.apellidop}</span>
-                <span class="uv-info">{user.rut}</span>
-                <span class="uv-info">{user.correo}</span>
-            </div>
-            
-            <div class="uv-body">
-                <div class="uv-sub-header">
-                    <span class="uv-subsidies">Mis Subsidios</span>
-                    <button class="sub-btn btn btn-primary">Explorar</button>
-                </div>
-                <div class="sub-group">
-                    <div class="sub">
-                        <span class="sub-name">Nombre Subsidio</span>
-                        <span class="sub-body">Aquí va la descripción del subsidio</span>
-                    </div>
-                </div>
-            </div>
+      <div class="uv-card card m-3">
+        <span class="uv-name">
+          Bienvenido {user.pnombre} {user.apellidop}
+        </span>
+        <span class="uv-info">{user.rut}</span>
+        <span class="uv-info">{user.correo}</span>
+      </div>
+
+      <div class="uv-body">
+        <div class="uv-sub-header">
+          <span class="uv-subsidies">Mis Subsidios</span>
+          <button class="sub-btn btn btn-primary">Postular</button>
         </div>
-    )
+
+        {/*Tarjetas de subsidios */}
+
+        <div class="sub-group">
+          <div class="sub">
+            <span class="sub-name">Nombre Subsidio</span>
+            <div class="sub-body-cont">
+                <p class="sub-body">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+                convallis id mi eget vestibulum. Suspendisse id mauris quis sapien
+                aliquet gravida.
+                </p>
+            </div>
+            <div class="approve-btn">
+              <svg width="24" height="24" viewBox="0 0 24 24">
+                <path d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z" />
+              </svg>
+            </div>
+          </div>
+
+          <div class="sub">
+            <span class="sub-name">Nombre Subsidio</span>
+            <div class="sub-body-cont">
+                <p class="sub-body">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+                convallis id mi eget vestibulum. Suspendisse id mauris quis sapien
+                aliquet gravida.
+                </p>
+            </div>
+            <div class="approve-btn">
+              <svg width="24" height="24" viewBox="0 0 24 24">
+                <path d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z" />
+              </svg>
+            </div>
+          </div>
+
+          <div class="sub">
+            <span class="sub-name">Nombre Subsidio</span>
+            <div class="sub-body-cont">
+                <p class="sub-body">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+                convallis id mi eget vestibulum. Suspendisse id mauris quis sapien
+                aliquet gravida.
+                </p>
+            </div>
+            <div class="approve-btn">
+              <svg width="24" height="24" viewBox="0 0 24 24">
+                <path d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z" />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
