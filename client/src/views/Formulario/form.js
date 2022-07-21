257ce89b1bc8479eb43cd76b7047eeeb35541e57
Ragnalex <66427660+Ragnalex@users.noreply.github.com>
2022-07-21 17:39:02 -0400

    Arreglo form de documentos, variables de recepcion

--- client/src/views/Formulario/form.js
+++ client/src/views/Formulario/form.js
@@ -1,37 +1,83 @@
 import "./form.css";
-import React from "react";
+import { React, useContext, useState } from "react";
+import { Context } from "../../context/Context";
+
 
 export default function Formulario() {
+
+  const { user } = useContext(Context);     //Usuario actual logeado
+
+  const [cedulaForm, setCedulaForm] = useState("");     //Var de Recepcion de documentos
+  const [certNacForm, setCertNac] = useState("");
+  const [certCivil, setCertCivil] = useState("");
+  const [libAhForm, setLibAh] = useState("");
+  const [certRSH, setCertRSH] = useState("");
+  const [RepMun, setRepMun] = useState("");
+  const [certAvaluo, setCertAvaluo] = useState("");
+  const [Escritura, setEscritura] = useState("");
+
+  const handleFormulario = (e) => {
+    e.preventDefault();
+
+  }
+
   return (
-    <div class="form-container">
-      <div class="form">
+    <div class="">
+      <form class="form" onSubmit={handleFormulario}>
         <h1 class="text-center p-5"></h1>
 
-        <div class="form-card card mt-3">
+        <div class="form-card card mt-3 ml-5">
           <span class="form-title text-center">Formulario</span>
-          <span class="form-req">Fotocopia de Cédula de Identidad</span>
 
+          <div class="mb-3">
+          <label for="formFile" className="form-req">Fotocopia de Cédula de Identidad</label>
+          <input class="form-control" type="file" onChange={e=>setCedulaForm(e.target.value)}/>
+
+          <label for="formFile" className="form-req">Certificado nacimiento</label>
+          <input class="form-control" type="file" onChange={e=>setCertNac(e.target.value)}/>
+
+          <label for="formFile" className="form-req">Certificado Estado Civil</label>
+          <input class="form-control" type="file"onChange={e=>setCertCivil(e.target.value)}/>
+
+          <label for="formFile" className="form-req">Libreta de ahorro</label>
+          <input class="form-control" type="file" onChange={e=>setLibAh(e.target.value)}/>
+
+          <label for="formFile" className="form-req">Certificado Registro Social de Hogares</label>
+          <input class="form-control" type="file" onChange={e=>setCertRSH(e.target.value)}/>
+
+          <label for="formFile" className="form-req">Recepcion Municipal</label>
+          <input class="form-control" type="file" onChange={e=>setRepMun(e.target.value)}/>
+
+          <label for="formFile" className="form-req">Certificado Avaluo Fiscal</label>
+          <input class="form-control" type="file" onChange={e=>setCertAvaluo(e.target.value)}/>
+
+          <label for="formFile" className="form-req">Escritura Vivienda</label>
+          <input class="form-control" type="file" onChange={e=>setEscritura(e.target.value)}/>
+
+          </div>
+
+          {/*
           <button class="upload-area">
             <span class="upload-icon">
-            <svg width="40" height="40" viewBox="0 0 24 24"><path d="M19.35,10.04C18.67,6.59 15.64,4 12,4C9.11,4 6.6,5.64 5.35,8.04C2.34,8.36 0,10.91 0,14A6,6 0 0,0 6,20H19A5,5 0 0,0 24,15C24,12.36 21.95,10.22 19.35,10.04M19,18H6A4,4 0 0,1 2,14C2,11.95 3.53,10.24 5.56,10.03L6.63,9.92L7.13,8.97C8.08,7.14 9.94,6 12,6C14.62,6 16.88,7.86 17.39,10.43L17.69,11.93L19.22,12.04C20.78,12.14 22,13.45 22,15A3,3 0 0,1 19,18M8,13H10.55V16H13.45V13H16L12,9L8,13Z" /></svg>
+            <svg width="15" height="15" viewBox="0 0 20 20"><path d="M19.35,10.04C18.67,6.59 15.64,4 12,4C9.11,4 6.6,5.64 5.35,8.04C2.34,8.36 0,10.91 0,14A6,6 0 0,0 6,20H19A5,5 0 0,0 24,15C24,12.36 21.95,10.22 19.35,10.04M19,18H6A4,4 0 0,1 2,14C2,11.95 3.53,10.24 5.56,10.03L6.63,9.92L7.13,8.97C8.08,7.14 9.94,6 12,6C14.62,6 16.88,7.86 17.39,10.43L17.69,11.93L19.22,12.04C20.78,12.14 22,13.45 22,15A3,3 0 0,1 19,18M8,13H10.55V16H13.45V13H16L12,9L8,13Z" /></svg>
             </span>
 
-            <span class="upload-title">
+            {/*<span class="upload-title">
               Arrastre y suelte aquí para cargar el archivo
             </span>
 
             <span class="upload-desc">
               O, puedes seleccionar un archivo haciendo click <strong>aquí</strong>
-            </span>
-          </button>
+          </span>
+          </button>*/}
 
           <div class="form-footer">
             <button class="close-btn btn">Atrás</button>
-            <button class="submit-btn btn btn-primary">Enviar</button>
+            <button class="submit-btn btn btn-primary" type="submit">Enviar</button>
           </div>
 
         </div>
-      </div>
+      </form>
     </div>
   );
 }
