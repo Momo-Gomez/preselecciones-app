da3c705481e4a2cc579b87e110717487c8e88b2c
Conejo <vf.araya.a@gmail.com>
2022-07-22 11:59:12 -0400

    Update Admin.js

--- client/src/views/Admin/Admin.js
+++ client/src/views/Admin/Admin.js
@@ -2,10 +2,29 @@ import React, { Fragment } from "react";
 import "./Admin.css";
 import { useContext } from "react";
 import { Context } from "../../context/Context";
+import axios from "axios";
+
+var postulantes = [];
 
 
 const Admin = () => {
   const { user } = useContext(Context); // para mostrar los datos del usuario
+
+  const handlePostulantes = async () => {
+    try {
+      
+      const res = await axios.get("http://localhost:5000/api/admin/postulaciones");       //optiene un array con todos los postulantes
+      postulantes = JSON.stringify(res.data);
+      console.log(postulantes);
+
+    } catch (error) {
+      console.log(error);
+    }
+   
+
+  }
+
+
   return (
     
     <div class="uv-container">
@@ -16,12 +35,13 @@ const Admin = () => {
               <span class="uv-name">Bienvenido {user.pnomombre} {user.apellidop}</span>
         
             </div>
-
+            
             <div class="list-group shadow">
+              <button onClick={handlePostulantes} type="button" class="list-group-item list-group-item-action">Actualizar</button>
               <h class="list-group-item list-group-item-action active text-bg-secondary" aria-current="true">Postulantes (Click sobre el nombre para descargar archivos)</h>
-              <button type="button" class="list-group-item list-group-item-action">Bernardo Millapan/19.468.817-2</button>
+              <button type="button" class="list-group-item list-group-item-action">postulantes</button>
               <button type="button" class="list-group-item list-group-item-action">Juanitos Lechuga/20.342.123-5</button>
-              <button type="button" class="list-group-item list-group-item-action">Juanin juanjarri/19.343.095-2</button>
+              <button type="button" class="list-group-item list-group-item-action">{postulantes[0]}</button>
             </div>
     </div>      
     
